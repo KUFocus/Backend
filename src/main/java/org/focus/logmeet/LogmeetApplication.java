3893cf6f4b89840cbad2d7dc641b3495719928c6
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-05-20 02:44:57 +0900

    feat: EnableJpaAuditing annotation 추가

--- src/main/java/org/focus/logmeet/LogmeetApplication.java
+++ src/main/java/org/focus/logmeet/LogmeetApplication.java
@@ -2,8 +2,10 @@ package org.focus.logmeet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class LogmeetApplication {
 
 	public static void main(String[] args) {
