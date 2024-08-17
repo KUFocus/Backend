75869005d88e365a502036110cb1521fd1b44287
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-08-18 03:22:30 +0900

    fix: 인증받지 않은 유저가 프로젝트 생성 시 예외 발생

--- src/main/java/org/focus/logmeet/service/ProjectService.java
+++ src/main/java/org/focus/logmeet/service/ProjectService.java
@@ -2,6 +2,8 @@ package org.focus.logmeet.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.focus.logmeet.common.exception.BaseException;
+import org.focus.logmeet.common.response.BaseExceptionResponseStatus;
 import org.focus.logmeet.controller.dto.Project.ProjectCreateRequest;
 import org.focus.logmeet.controller.dto.Project.ProjectCreateResponse;
 import org.focus.logmeet.domain.Project;
@@ -15,13 +17,14 @@ import org.focus.logmeet.security.aspect.CurrentUserHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.focus.logmeet.common.response.BaseExceptionResponseStatus.USER_NOT_AUTHENTICATED;
 import static org.focus.logmeet.domain.enums.Role.LEADER;
 import static org.focus.logmeet.domain.enums.Status.ACTIVE;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class ProjectService {
+public class ProjectService { //TODO: 인증 과정 중 예외 발생 시 BaseException 으로 처리
 
     private final ProjectRepository projectRepository;
     private final UserProjectRepository userProjectRepository;
@@ -31,6 +34,10 @@ public class ProjectService {
     public ProjectCreateResponse createProject(ProjectCreateRequest request) {
         log.info("프로젝트 생성 시도: projectName={}, projectContent={}", request.getName(), request.getContent());
         User currentUser = CurrentUserHolder.get();
+
+        if (currentUser == null) {
+            throw new BaseException(USER_NOT_AUTHENTICATED);
+        }
 
         Project project = Project.builder()
                 .name(request.getName())
