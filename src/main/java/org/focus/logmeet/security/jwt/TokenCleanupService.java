0a1d8d08a2ea3c1ffdf544645bd59b2387555165
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-08-18 03:19:28 +0900

    feat: 만료된 Refresh Token repository에서 자동 삭제되도록 Scheduler 설정

--- src/main/java/org/focus/logmeet/security/jwt/TokenCleanupService.java
+++ src/main/java/org/focus/logmeet/security/jwt/TokenCleanupService.java
@@ -1,2 +1,27 @@
-package org.focus.logmeet.security.jwt;public class TokenCleanupService {
+package org.focus.logmeet.security.jwt;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.focus.logmeet.repository.RefreshTokenRepository;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class TokenCleanupService {
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    @Scheduled(cron = "0 0 0 * * ?")  // 매일 자정에 실행
+    public void cleanUpExpiredTokens() {
+        log.info("만료된 Refresh Token 정리 작업 시작");
+        LocalDateTime now = LocalDateTime.now();
+        refreshTokenRepository.findByExpirationDateBefore(now).forEach(token -> {
+            refreshTokenRepository.delete(token);
+            log.info("만료된 Refresh Token 삭제: {}", token.getRefreshToken());
+        });
+        log.info("만료된 Refresh Token 정리 작업 완료");
+    }
 }
