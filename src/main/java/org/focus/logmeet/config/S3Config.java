381c8bfd5b08f949270db70fe9dc85d16d2fb28a
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-08-29 14:24:18 +0900

    feat: S3 (object storage) 적용 및 저장 로직 구현

--- src/main/java/org/focus/logmeet/config/S3Config.java
+++ src/main/java/org/focus/logmeet/config/S3Config.java
@@ -1,2 +1,35 @@
-package org.focus.logmeet.config;public class S3Config {
+package org.focus.logmeet.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+
+    @Value("${cloud.naver.credentials.accessKey}")
+    private String accessKey;
+
+    @Value("${cloud.naver.credentials.secretKey}")
+    private String secretKey;
+
+    @Bean
+    public AmazonS3 amazonS3() {
+        String endPoint = "https://kr.object.ncloudstorage.com";
+        String regionName = "kr-standard";
+
+        try {
+            return AmazonS3ClientBuilder.standard()
+                    .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endPoint, regionName))
+                    .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
+                    .build();
+        } catch (Exception e) {
+            throw new RuntimeException("S3 클라이언트 생성 중 오류가 발생했습니다.", e);
+        }
+    }
 }
--- src/main/java/org/focus/logmeet/service/S3Service.java
+++ src/main/java/org/focus/logmeet/service/S3Service.java
@@ -1,14 +1,11 @@
 package org.focus.logmeet.service;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
+import org.focus.logmeet.common.exception.BaseException;
+import org.focus.logmeet.common.response.BaseExceptionResponseStatus;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -20,30 +17,15 @@ public class S3Service {
 
     private final AmazonS3 s3;
 
-    @Value("${cloud.naver.credentials.accessKey}")
-    private String accessKey;
-
-    @Value("${cloud.naver.credentials.secretKey}")
-    private String secretKey;
-
-    public S3Service() {
-        String endPoint = "https://kr.object.ncloudstorage.com";
-        String regionName = "kr-standard";
-
-        this.s3 = AmazonS3ClientBuilder.standard()
-                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endPoint, regionName))
-                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
-                .build();
-    }
-
     public void uploadFile(String directory, String objectName, File file) {
         try {
             String bucketName = "logmeet";
             String fullObjectName = directory + "/" + objectName;
             s3.putObject(new PutObjectRequest(bucketName, fullObjectName, file));
-            log.info("버킷에 객체 업로드 시도: bucketName={}, ObjectName={}", bucketName, fullObjectName);
+            log.info("버킷에 객체 업로드 성공: bucketName={}, ObjectName={}", bucketName, fullObjectName);
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("S3에 파일 업로드 중 오류 발생: directory={}, objectName={}", directory, objectName, e);
+            throw new BaseException(BaseExceptionResponseStatus.S3_FILE_UPLOAD_ERROR);
         }
     }
 }
