e529735068a9e6c84d0a0a40902f7cf67874acf9
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-09-12 16:32:18 +0900

    feat: 회의록에 TEMP 상태 추가 및 스케줄링으로 회의록 관리하는 로직 구현

--- src/main/java/org/focus/logmeet/repository/MinutesRepository.java
+++ src/main/java/org/focus/logmeet/repository/MinutesRepository.java
@@ -1,7 +1,15 @@
 package org.focus.logmeet.repository;
 
 import org.focus.logmeet.domain.Minutes;
+import org.focus.logmeet.domain.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface MinutesRepository extends JpaRepository<Minutes, Long> {
+    @Query("SELECT m FROM Minutes m WHERE m.status = :status AND m.createdAt <= :timeLimit")
+    List<Minutes> findOldTemporaryMinutes(@Param("status") Status status, @Param("timeLimit") LocalDateTime timeLimit);
 }
--- src/main/java/org/focus/logmeet/service/MinutesService.java
+++ src/main/java/org/focus/logmeet/service/MinutesService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.focus.logmeet.common.exception.BaseException;
 import org.focus.logmeet.controller.dto.minutes.MinutesCreateResponse;
+import org.focus.logmeet.controller.dto.minutes.MinutesFileUploadResponse;
 import org.focus.logmeet.domain.Minutes;
 import org.focus.logmeet.domain.Project;
 import org.focus.logmeet.domain.enums.MinutesType;
@@ -15,6 +16,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.MultipartBodyBuilder;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.MultiValueMap;
@@ -25,12 +27,15 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URI;
+import java.time.LocalDateTime;
 import java.util.Base64;
+import java.util.List;
 import java.util.Objects;
 
 import static org.focus.logmeet.common.response.BaseExceptionResponseStatus.*;
-import static org.focus.logmeet.domain.enums.MinutesType.*;
+import static org.focus.logmeet.domain.enums.MinutesType.MANUAL;
 import static org.focus.logmeet.domain.enums.Status.ACTIVE;
+import static org.focus.logmeet.domain.enums.Status.TEMP;
 
 @Slf4j
 @Service
@@ -42,32 +47,80 @@ public class MinutesService {
     private final ProjectRepository projectRepository;
     private final RestTemplate restTemplate;
 
-    // 음성을 업로드 하고, 텍스트 추출 후 회의록 저장
+    // 일정 시간이 지난 임시 회의록을 삭제하는 스케줄러를 추가
+    @Scheduled(fixedRate = 1800000)
     @Transactional
-    public MinutesCreateResponse processAndUploadVoice(String base64FileData, String minutesName, String fileName, Long projectId) {
-        log.info("음성 업로드로 회의록 생성 시도: minutesName={}", minutesName);
-        Project project = projectRepository.findById(projectId)
-                .orElseThrow(() -> new BaseException(PROJECT_NOT_FOUND));
+    public void deleteOldTemporaryMinutes() {
+        LocalDateTime thirtyMinutesAgo = LocalDateTime.now().minusMinutes(30);
+        List<Minutes> oldTemporaryMinutes = minutesRepository.findOldTemporaryMinutes(TEMP, thirtyMinutesAgo);
+        for (Minutes minutes : oldTemporaryMinutes) {
+            log.info("삭제할 임시 회의록: minutesId={}, createdAt={}", minutes.getId(), minutes.getCreatedAt());
+            minutesRepository.delete(minutes);
+        }
+    }
 
-        // 새로운 Minutes 엔티티 생성
+    // 파일 업로드 후 임시 회의록 생성
+    @Transactional
+    public MinutesFileUploadResponse uploadFile(String base64FileData, String fileName, MinutesType fileType) {
+        log.info("파일 업로드로 임시 회의록 생성 시도: fileType={}", fileType);
+
+        // 새로운 임시 상태의 Minutes 엔티티 생성
         Minutes minutes = new Minutes();
-        minutes.setProject(project);
-        minutes.setName(minutesName);
-        minutes.setType(VOICE);
+        minutes.setType(fileType);
+        minutes.setStatus(TEMP);  // 임시 상태로 설정
 
         File tempFile = decodeBase64ToFile(base64FileData, fileName);
 
+        switch (fileType) {
+            case VOICE:
+                uploadToS3AndProcessVoice(tempFile, fileName, minutes);
+                break;
+
+            case PICTURE:
+                uploadToS3ForPicture(tempFile, fileName, minutes);
+                break;
+
+            default:
+                throw new BaseException(MINUTES_TYPE_NOT_FOUND);
+        }
+        log.info("임시 회의록 저장 시도: fileType={}", fileType);
+        // 임시 회의록 저장
+        minutesRepository.save(minutes);
+        log.info("임시 회의록 저장 완료: minutesId={}, fileType={}", minutes.getId(), fileType);
+
+        return new MinutesFileUploadResponse(minutes.getVoiceFilePath(), minutes.getType());
+    }
+
+    // 임시 파일을 S3에 업로드 및 Flask 서버에 텍스트 변환 요청 처리
+    private void uploadToS3AndProcessVoice(File tempFile, String fileName, Minutes minutes) {
         try {
-            // 음성 파일을 S3에 업로드
             s3Service.uploadFile("minutes_voice", fileName, tempFile);
             minutes.setVoiceFilePath("minutes_voice/" + fileName);
+
+            String content = processVoiceFile(tempFile, fileName);  // 텍스트 변환 요청
+            minutes.setContent(content);
         } catch (Exception e) {
-            log.error("Object Storage에 음성 파일 업로드 중 오류 발생", e);
+            log.error("음성 파일 업로드 또는 텍스트 처리 중 오류 발생", e);
             throw new BaseException(MINUTES_VOICE_FILE_UPLOAD_ERROR);
         }
+    }
+
+    // 사진 파일을 S3에 업로드하는 로직
+    private void uploadToS3ForPicture(File tempFile, String fileName, Minutes minutes) {
+        try {
+            s3Service.uploadFile("minutes_photo", fileName, tempFile);
+            minutes.setPhotoFilePath("minutes_photo/" + fileName);
+        } catch (Exception e) {
+            log.error("사진 파일 업로드 중 오류 발생", e);
+            throw new BaseException(MINUTES_PHOTO_FILE_UPLOAD_ERROR);
+        }
+    }
+
+    // 음성 파일을 Flask 서버에 전송하여 텍스트 변환하는 메서드
+    private String processVoiceFile(File tempFile, String fileName) {
+        log.info("음성 파일 텍스트 변환 시도: fileName={}", fileName);
 
         try {
-            // 파일을 Flask 서버에 전송
             MultipartBodyBuilder builder = new MultipartBodyBuilder();
             builder.part("file", new FileSystemResource(tempFile));
             builder.part("fileName", fileName);
@@ -82,53 +135,33 @@ public class MinutesService {
                     .build().toUri();
 
             ResponseEntity<String> response = restTemplate.postForEntity(uri, requestEntity, String.class);
-
-            log.info("Flask 서버로부터 변환된 텍스트 수신: {}", response.getBody());
-
-            // 변환된 텍스트를 Minutes 객체에 설정
-            minutes.setContent(Objects.requireNonNull(response.getBody()));
-
-            // 새로운 Minutes 엔티티 저장
-            minutesRepository.save(minutes);
+            log.info("음성 파일 텍스트 변환 성공: fileName={}", fileName);
+            return Objects.requireNonNull(response.getBody());
 
         } catch (Exception e) {
-            log.error("텍스트 처리 중 오류 발생", e);
+            log.error("음성 파일 텍스트 변환 중 오류 발생", e);
             throw new BaseException(MINUTES_TEXT_FILE_UPLOAD_ERROR);
         }
-        log.info("음성 업로드로 회의록 생성 성공: minutesName={}", minutesName);
-
-        return new MinutesCreateResponse(minutes.getId(), minutes.getProject().getId());
     }
 
-    // 사진을 업로드 하고, 회의록 저장
-    public MinutesCreateResponse uploadPhoto(String base64FileData, String minutesName, String fileName, Long projectId) {
-        log.info("사진 업로드로 회의록 생성 시도: minutesName={}", minutesName);
+    // 회의록 정보 업데이트
+    @Transactional
+    public MinutesCreateResponse updateMinutesInfo(Long minutesId, String minutesName, Long projectId) {
+        log.info("회의록 정보 업데이트 시도: minutesId={}, minutesName={}, projectId={}", minutesId, minutesName, projectId);
+
+        Minutes minutes = minutesRepository.findById(minutesId)
+                .orElseThrow(() -> new BaseException(MINUTES_NOT_FOUND));
 
         Project project = projectRepository.findById(projectId)
                 .orElseThrow(() -> new BaseException(PROJECT_NOT_FOUND));
 
-        Minutes minutes = new Minutes();
-        minutes.setProject(project);
         minutes.setName(minutesName);
-        minutes.setType(PICTURE);
+        minutes.setProject(project);
+        minutes.setStatus(ACTIVE);  // ACTIVE 상태로 변경
 
-        File tempFile = decodeBase64ToFile(base64FileData, fileName);
-
-        try {
-            // 사진 파일을 S3에 업로드
-            s3Service.uploadFile("minutes_photo", fileName, tempFile);
-            minutes.setPhotoFilePath("minutes_photo/" + fileName);
-        } catch (Exception e) {
-            log.error("Object Storage에 사진 파일 업로드 중 오류 발생", e);
-            throw new BaseException(MINUTES_PHOTO_FILE_UPLOAD_ERROR);
-        }
-
-        // Minutes 객체 저장
         minutesRepository.save(minutes);
 
-        log.info("사진 업로드로 회의록 생성 성공: minutesName={}", minutesName);
-
-        return new MinutesCreateResponse(minutes.getId(), minutes.getProject().getId());
+        return new MinutesCreateResponse(minutes.getId(), project.getId());
     }
 
     // 수동 입력된 회의록을 저장
@@ -162,6 +195,7 @@ public class MinutesService {
         try (FileOutputStream fos = new FileOutputStream(tempFile)) {
             fos.write(decodedBytes);
         } catch (IOException e) {
+            log.error("파일 디코딩 중 오류 발생: {}", e.getMessage());
             throw new BaseException(S3_FILE_DECODING_ERROR);
         }
         return tempFile;
