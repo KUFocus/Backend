886d295b5fa16f07bc5e1bd3efe433ca687d6147
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-09-30 21:41:45 +0900

    fix: 액세스 토큰 유지 시간 수정

--- src/main/java/org/focus/logmeet/security/jwt/JwtProvider.java
+++ src/main/java/org/focus/logmeet/security/jwt/JwtProvider.java
@@ -32,7 +32,7 @@ public class JwtProvider {
     private final UserDetailsServiceImpl userDetailsService;
     private final RefreshTokenRepository refreshTokenRepository;
 
-    private static final long ACCESS_TIME = 15 * 60 * 1000L; // 15분
+    private static final long ACCESS_TIME = 1440 * 60 * 1000L; // 24시간
     private static final long REFRESH_TIME = 7 * 24 * 60 * 60 * 1000L; // 7일
 
     @Value("${secret.jwt-secret-key}")
