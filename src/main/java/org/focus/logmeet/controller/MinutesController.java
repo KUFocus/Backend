cff9bd94ebb1fe2042eb928f02029a17af74c829
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-10-07 19:56:29 +0900

    comment: 컨트롤러에 비즈니스 로직에 대한 설명 추가

--- src/main/java/org/focus/logmeet/controller/MinutesController.java
+++ src/main/java/org/focus/logmeet/controller/MinutesController.java
@@ -15,6 +15,11 @@ public class MinutesController {
 
     private final MinutesService minutesService;
 
+    /**
+     * 파일로 생성된 회의록의 이름과 프로젝트 정보를 업데이트한다.
+     * @param request 업데이트할 회의록의 ID, 이름, 프로젝트 ID를 포함
+     * @return 업데이트된 회의록 정보 (MinutesCreateResponse)
+     */
     @PutMapping("/update-info")
     public BaseResponse<MinutesCreateResponse> updateMinutesInfo(@RequestBody MinutesInfoCreateRequest request) {
         log.info("회의록 정보 설정: minutesName={}, projectId={}", request.getMinutesName(), request.getProjectId());
@@ -22,20 +27,34 @@ public class MinutesController {
         return new BaseResponse<>(response);
     }
 
-    @PostMapping("/upload-file") //TODO: 동일명 파일 업로드 시 덮어쓰는 문제 해결
+    /**
+     * 음성 또는 사진 파일을 업로드하여 회의록을 생성한다.
+     * @param request base64 인코딩된 파일 데이터, 파일 이름, 파일 타입을 포함
+     * @return 생성된 회의록의 파일 경로와 타입 정보 (MinutesFileUploadResponse)
+     */
+    @PostMapping("/upload-file") //TODO: 동일 파일명 업로드 시 덮어쓰는 문제 처리 필요
     public BaseResponse<MinutesFileUploadResponse> uploadFile(@RequestBody MinutesFileUploadRequest request) {
         log.info("파일 업로드 요청: fileName={}, fileType={}", request.getFileName(), request.getFileType());
         MinutesFileUploadResponse response = minutesService.uploadFile(request.getBase64FileData(), request.getFileName(), request.getFileType());
         return new BaseResponse<>(response);
     }
 
-    //TODO: 텍스트 요약 요청 보완 필요
-    @PostMapping("/summarize-text")
+    /**
+     * 회의록의 텍스트 내용을 요약한다.
+     * @param request 요약할 텍스트를 포함한 요청
+     * @return 요약된 텍스트 정보 (MinutesSummarizeResponse)
+     */
+    @PostMapping("/summarize-text") //TODO: 텍스트 요약 API 요청 보완 필요
     public BaseResponse<MinutesSummarizeResponse> summarizeText(@RequestBody MinutesSummarizeRequest request) {
         log.info("텍스트 요약 요청: extractedText={}", request.getExtractedText());
         return null;
     }
 
+    /**
+     * 텍스트를 직접 입력하여 수동으로 회의록을 생성한다.
+     * @param request 회의록 이름, 프로젝트 ID, 입력된 텍스트 내용을 포함
+     * @return 생성된 회의록 정보 (MinutesCreateResponse)
+     */
     @PostMapping("/upload-content")
     public BaseResponse<MinutesCreateResponse> uploadManualEntry(@RequestBody MinutesManuallyCreateRequest request) {
         log.info("직접 작성한 회의록 업로드 요청: minutesName={}, projectId={}", request.getMinutesName(), request.getProjectId());
@@ -43,6 +62,11 @@ public class MinutesController {
         return new BaseResponse<>(response);
     }
 
+    /**
+     * 단일 회의록의 상세 정보를 조회한다.
+     * @param minutesId 조회할 회의록의 ID
+     * @return 조회된 회의록의 세부 정보 (MinutesInfoResult)
+     */
     @GetMapping("/{minutesId}")
     public BaseResponse<MinutesInfoResult> getMinutes(@PathVariable Long minutesId) {
         log.info("회의록 정보 요청: minutesId={}", minutesId);
