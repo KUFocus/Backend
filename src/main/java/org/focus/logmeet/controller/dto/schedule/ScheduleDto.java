ced1249043b90c0e6bd624d0860223cb2235b9c5
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-10-26 19:48:41 +0900

    fix: 테스트 코드를 위해 MinutesSummarizeResult, ScheduleDto에 애노테이션 추가

--- src/main/java/org/focus/logmeet/controller/dto/schedule/ScheduleDto.java
+++ src/main/java/org/focus/logmeet/controller/dto/schedule/ScheduleDto.java
@@ -2,9 +2,13 @@ package org.focus.logmeet.controller.dto.schedule;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ScheduleDto {
     private String extractedScheduleDate;
     private String extractedScheduleContent;
