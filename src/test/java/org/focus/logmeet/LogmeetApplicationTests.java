99050847d6a3e22d094e414e9c52b26c59a28fbe
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-11-10 21:54:48 +0900

    test(LogmeetApplicationTests): 테스트 코드 작성

--- src/test/java/org/focus/logmeet/LogmeetApplicationTests.java
+++ src/test/java/org/focus/logmeet/LogmeetApplicationTests.java
@@ -8,12 +8,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 @SpringBootTest
 class LogmeetApplicationTests {
 
 	@Test
 	@DisplayName("애플리케이션 컨텍스트가 성공적으로 로드된다")
 	void contextLoads() {
+	}
+
+	@Test
+	@DisplayName("애플리케이션 메인 메서드 호출 테스트")
+	void mainMethodTest() {
+		assertDoesNotThrow(() -> LogmeetApplication.main(new String[]{}));
 	}
 
 	@Configuration
