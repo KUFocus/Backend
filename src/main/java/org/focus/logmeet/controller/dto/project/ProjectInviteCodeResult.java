1b98c2c0db5ad7e41ade99a0d55df6c30863d338
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-11-19 23:14:34 +0900

    feat(ProjectInviteCodeResult): 초대 코드 결과 반환을 위한 DTO 생성

--- src/main/java/org/focus/logmeet/controller/dto/project/ProjectInviteCodeResult.java
+++ src/main/java/org/focus/logmeet/controller/dto/project/ProjectInviteCodeResult.java
@@ -1,2 +1,14 @@
-package org.focus.logmeet.controller.dto.project;public class ProjectInviteCodeResult {
+package org.focus.logmeet.controller.dto.project;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class ProjectInviteCodeResult {
+    private Long projectId;
+    private String inviteCode;
+    private LocalDateTime expirationDate;
 }
