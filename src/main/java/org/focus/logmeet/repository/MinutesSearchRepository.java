d38c9d923fbd7d00cc0cb96b176222233db08324
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-11-27 21:17:08 +0900

    feat(MinutesSearchRepository): 쿼리에 따른 회의록 검색 결과를 반환해주는 elastic 검색 쿼리 정의

--- src/main/java/org/focus/logmeet/repository/MinutesSearchRepository.java
+++ src/main/java/org/focus/logmeet/repository/MinutesSearchRepository.java
@@ -1,2 +1,30 @@
-package org.focus.logmeet.repository;public interface MinutesSearchRepository {
+package org.focus.logmeet.repository;
+
+import org.focus.logmeet.domain.elasticsearch.MinutesDocument;
+import org.springframework.data.elasticsearch.annotations.Query;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import java.util.List;
+
+public interface MinutesSearchRepository extends ElasticsearchRepository<MinutesDocument, Long> {
+        @Query("""
+                {
+                  "bool": {
+                    "must": [
+                      {
+                        "terms": { "id": ?1 }
+                      },
+                      {
+                        "bool": {
+                          "should": [
+                            { "match": { "title": "?0" } },
+                            { "match": { "content": "?0" } }
+                          ]
+                        }
+                      }
+                    ]
+                  }
+                }
+                """)
+    List<MinutesDocument> searchByQuery(String query, List<Long> ids);
 }
