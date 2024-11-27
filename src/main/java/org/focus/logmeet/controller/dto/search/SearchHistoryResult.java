82742eb96d02c2c09df68348437dc7f680c5fa06
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-11-27 21:07:20 +0900

    feat(SearchHistoryResult): 검색 기록 조회 결과 dto 생성

--- src/main/java/org/focus/logmeet/controller/dto/search/SearchHistoryResult.java
+++ src/main/java/org/focus/logmeet/controller/dto/search/SearchHistoryResult.java
@@ -1,2 +1,15 @@
-package org.focus.logmeet.controller.dto.search;public class SearchHistoryResult {
+package org.focus.logmeet.controller.dto.search;
+
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SearchHistoryResult {
+    private String title;
+    private String projectName;
 }
