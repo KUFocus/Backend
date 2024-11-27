ed77733d87539d81485ac55450c37651c3e23586
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-11-27 21:05:46 +0900

    feat(MinutesSearchRequest): 검색 쿼리 요청 dto 생성

--- src/main/java/org/focus/logmeet/controller/dto/search/MinutesSearchRequest.java
+++ src/main/java/org/focus/logmeet/controller/dto/search/MinutesSearchRequest.java
@@ -1,2 +1,12 @@
-package org.focus.logmeet.controller.dto.search;public class MinutesSearchRequest {
+package org.focus.logmeet.controller.dto.search;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MinutesSearchRequest {
+    private String query;
 }
