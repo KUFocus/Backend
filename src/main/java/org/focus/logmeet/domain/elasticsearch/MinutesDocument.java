9c9dea981116a76d04a0862eb204c85cfd3d3895
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-11-27 21:14:59 +0900

    feat(MinutesDocument): 회의록 검색을 위한 elastic document 클래스 생성 및 한국어 tokenizer nori 적용

--- src/main/java/org/focus/logmeet/domain/elasticsearch/MinutesDocument.java
+++ src/main/java/org/focus/logmeet/domain/elasticsearch/MinutesDocument.java
@@ -1,2 +1,24 @@
-package org.focus.logmeet.domain.elasticsearch;public class MinutesDocument {
+package org.focus.logmeet.domain.elasticsearch;
+
+import jakarta.persistence.Id;
+import lombok.*;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Document(indexName = "minutes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MinutesDocument {
+    @Id
+    private Long id;
+
+    @Field(type = FieldType.Text, analyzer = "nori", searchAnalyzer = "standard")
+    private String title;
+
+    @Field(type = FieldType.Text, analyzer = "nori", searchAnalyzer = "standard")
+    private String content;
 }
